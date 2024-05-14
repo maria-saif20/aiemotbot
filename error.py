92e490efc0f48a3171f7f3014266af4644cb49cf
Maria saif <147303293+maria-saif20@users.noreply.github.com>
2024-05-14 17:50:34 +0500

    Update error.py

--- error.py
+++ error.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # Custom CSS
-with open('styles.css') as f:
+with open('style.css') as f:  # Change 'styles.css' to 'style.css'
     css = f.read()
 
 st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
@@ -11,7 +11,7 @@ st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
 # Show the logo and title side by side
 col1, col2 = st.columns([1, 4])
 with col1:
-    st.image("brainbot.png", width=100)
+    st.image("AIEMOTBOT.png", width=100)  # Change 'brainbot.png' to 'AIEMOTBOT.png'
 with col2:
     st.title("Error")
 
